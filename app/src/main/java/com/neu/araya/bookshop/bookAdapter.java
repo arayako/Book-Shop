8814022162d3fc76ca8dea54d823158e516f2275
araya <arayakodet111@gmail.com>
2016-06-21 16:58:50 +0700

    Book Adapter

--- app/src/main/java/com/neu/araya/bookshop/bookAdapter.java
+++ app/src/main/java/com/neu/araya/bookshop/bookAdapter.java
@@ -1,11 +1,19 @@
 package com.neu.araya.bookshop;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by iMac11 on 6/21/2016 AD.
  */
-public class bookAdapter {
+public class bookAdapter extends BaseAdapter{
 
     //Explicit
     private Context context;
@@ -20,4 +28,37 @@ public class bookAdapter {
         this.priceStrings = priceStrings;
     }  // Constructor
 
+    @Override
+    public int getCount() {
+        return nameStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = layoutInflater.inflate(R.layout.book_listview, viewGroup,false);
+
+        ImageView imageView = (ImageView) view1.findViewById(R.id.imageView2);
+        Picasso.with(context).load(iconStrings[i]).resize(150,180).into(imageView);
+
+        TextView nameTextView = (TextView)view1.findViewById(R.id.textView8);
+        nameTextView.setText(nameStrings[i]);
+
+        TextView priceTextView = (TextView) view1.findViewById(R.id.textView7);
+        priceTextView.setText(priceStrings[i]);
+
+
+        return view1;
+    }
 }  //Main class
