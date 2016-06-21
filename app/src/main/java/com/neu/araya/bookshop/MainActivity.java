8bb92c5cb850415f0ef4e8053854206915f138d6
araya <arayakodet111@gmail.com>
2016-06-21 14:31:03 +0700

    Syn JSON

--- app/src/main/java/com/neu/araya/bookshop/MainActivity.java
+++ app/src/main/java/com/neu/araya/bookshop/MainActivity.java
@@ -1,17 +1,25 @@
 package com.neu.araya.bookshop;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.speech.tts.Voice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     // Explicit
     private EditText userEditText, passwordEditText;
     private  String userString, passwordString;
-
+    private static final String urlJSON = "http://swiftcodingthai.com/neu/get_user.php";
 
 
     @Override
@@ -27,6 +35,46 @@ public class MainActivity extends AppCompatActivity {
 
     } // Main Method
 
+    //Create Inner Cless
+    private class MySynchronize extends AsyncTask<Void, Void, String> {
+
+        private Context context;
+        private  String urlString;
+
+        public MySynchronize(Context context, String urlString) {
+            this.context = context;
+            this.urlString = urlString;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlString).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+
+            }catch (Exception e) {
+                return null;
+            }
+
+        }  // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("BookShopV1", "JSON ==>" + s);
+
+        }  // onPost
+    }   // Class
+
+
     public void clickSignIn(View view){
 
         //Get Value from Edit text
@@ -36,14 +84,25 @@ public class MainActivity extends AppCompatActivity {
         if (userString.equals("") || passwordString.equals("")) {
             //Have Space
             MyAlert myAlert = new MyAlert();
-            myAlert.myDialog(this,"มีช่องว่าง","กรุณากรอกทุกช่องคะ");
+            myAlert.myDialog(this,"Have Space","Please Fill All Bank");
 
         } else {
+
+            searchUserAnPassword();
+
+
             //No space
 
 
         }
     }  // clickSignIn
+
+    private void searchUserAnPassword() {
+
+        MySynchronize mySynchronize = new MySynchronize(this, urlJSON);
+        mySynchronize.execute();
+
+    } //searchUser
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this,SignUpActivity.class));
